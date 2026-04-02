88df12f7835fe49cceeec4ddb313d09c431bff3b
lujun <lujuncm@163.com>
2026-04-02 17:11:12 +0000

    style(frontend): reposition TitleCard to bottom left

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
       <Background />
 
       {/* 左侧标题卡片 */}
-      <div className="absolute left-1/4 top-1/2 z-10 -translate-y-1/2">
+      <div className="absolute left-16 bottom-24 z-10">
         <TitleCard />
       </div>
 
